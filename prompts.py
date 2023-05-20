33de7947dc1a0e7f1fb603c9ad6ac29893e43aa9
Irakli <irakliy28@gmail.com>
2023-05-20 17:49:56 +0300

    minor changes

--- prompts.py
+++ prompts.py
@@ -3,7 +3,7 @@ class Prompt:
     topic = "computer architecture"
 
 
-    prompt_system = """ Assistant helps the users with theirquestions about uploaded files. Be brief in your answers.
+    prompt_system = """ You are assistant that helps users with their questions about uploaded files. Be brief in your answers.
                         Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. 
                         Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
                         For tabular information return it as an html table. Do not return markdown format.
@@ -12,7 +12,7 @@ class Prompt:
 
 
 
-    query_prompt_template = """ A new question asked by the user that needs to be answered by searching in a knowledge base about computer architecture.
+    query_prompt_template = """ A new question asked by the user that needs to be answered by searching in a knowledge base.
                                 Generate a search query based on the conversation and the new question. 
                                 Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
                                 Do not include any text inside [] or <<>> in the search query terms.
